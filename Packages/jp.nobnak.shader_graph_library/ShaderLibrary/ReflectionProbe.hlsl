ad9d44ef69710a784a83e8601b30f3b5c4b2dd0e
Nobu Nakata <nobu.nakata@gmail.com>
2025-06-05 18:36:31 +0900

    Add Environmental Light probe node

---
+++ Packages/jp.nobnak.shader_graph_library/ShaderLibrary/ReflectionProbe.hlsl
@@ -0,0 +1,28 @@
+#ifndef __PerceptualRoughnessToMipmapLevel__
+#define __PerceptualRoughnessToMipmapLevel__
+
+#ifndef SHADERGRAPH_PREVIEW
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+#endif
+
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/ImageBasedLighting.hlsl"
+
+void PerceptualRoughnessToMipmapLevel_float(float perceptualRoughness, out float mipmapLevel) {
+    mipmapLevel = PerceptualRoughnessToMipmapLevel(perceptualRoughness);
+}
+
+// https://discussions.unity.com/t/unity-6-reflection-probes-in-unlit-shader-graph/1554873/15
+void GetReflectionProbe_float(float3 ViewDirWS, float3 PositionWS, float3 NormalsWS, float2 NormalizedScreenSpaceUV, float Roughness, out float3 Cubemap)
+{
+#ifdef SHADERGRAPH_PREVIEW
+    Cubemap = 0;
+#else
+    half3 reflectionVector = reflect(-normalize(ViewDirWS), normalize(NormalsWS));
+    half perceptualRoughness = Roughness;
+    float occlusion = 1;
+
+    Cubemap = GlossyEnvironmentReflection(reflectionVector, PositionWS, perceptualRoughness, occlusion, NormalizedScreenSpaceUV);
+#endif
+}
+
+#endif
